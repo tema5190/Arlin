0a363ba43c84acce1a0624aedb9015795161cd97
Derid Artem <tema5190@gmail.com>
2016-06-07 18:01:10 +0300

    07.06.2016

--- search/views.py
+++ search/views.py
@@ -4,14 +4,20 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
 
+    context = {}
+    context['list'] = [1,2,3,4,5,6,7,8]
     search_query = request.GET.get("input")
 
     print(search_query)
 
-    return render(request,'home.html')
+    return render(request,'home.html', context)
 
 def index_url(request):
     return render(request,'index_url.html')
 
 def known_urls(request):
     return render(request,'known_urls.html')
+
+def results(request):
+
+    return render(request,'results.html')
